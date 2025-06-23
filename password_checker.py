199a1d0bdb5a548ac398f1a1e158144f40fb64aa
Scott Rayner <scott.rayner10@gmail.com>
2025-06-23 19:41:35 +0100

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -10,27 +10,27 @@ def check_password_strength(password):
   if len(password) >= 8: # checking the length of the password to ensure it is 8 characters or above.
     score += 1
   else:
-    print("Password must be at least 8 characters long.")
+    print("Your password should be at least 8 characters long.")
   
   if re.search(r"[A-Z]", password): # Checking the password has an uppercase character.
     score += 1
   else: 
-    print("Password must contain at least 1 uppercase character.")
+    print("Your password should contain at least 1 uppercase character.")
   
   if re.search(r"[a-z]", password): # Checking the password has a lowercase character.
     score += 1
   else:
-    print("Password must contain at least 1 lowercase character.")
+    print("Your password should contain at least 1 lowercase character.")
   
   if re.search(r"/d", password): # Checking the password has a number.
     score += 1
   else:
-    print("Password must contain at least 1 number.")
+    print("Your password should contain at least 1 number.")
   
   if re.search(r"[!@#$^%&*(),.?\":{}<>]", password): # Checking the password has a special character.
     score += 1
   else:
-    print("Password must contain at least 1 special character !@#$^%&*(),.?\":{}<>")
+    print("Your password should contain at least 1 special character !@#$^%&*(),.?\":{}<>")
   
   return score
 
